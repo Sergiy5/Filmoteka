e145136a707fd24ae77fdb7e736d81624062422f
Serhii Kushnir <s1983kushnir@gmail.com>
2024-02-28 09:24:35 +0100

    Clear main index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,4 @@
 import './sass/index.scss';
-import './js/api/apiUrl';
-import './js/api/api-service';
 import './js/gallery/galleryHome'
 import './js/gallery/gallaryLibrary'
-import './js/gallery'
 import './js/modal/modal';
-import './js/gallery/genresList';
-import './js/refs';
-
